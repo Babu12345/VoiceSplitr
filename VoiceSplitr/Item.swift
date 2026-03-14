3affe1879adbb66a501edd5f77d1edc3ef84d7e3
Babs <bwanyeki12@gmail.com>
2026-03-14 13:11:50 -0400

    Initial Commit

---
+++ VoiceSplitr/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  VoiceSplitr
+//
+//  Created by Babu Wanyeki on 3/14/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ VoiceSplitrTests/VoiceSplitrTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceSplitrTests.swift
+//  VoiceSplitrTests
+//
+//  Created by Babu Wanyeki on 3/14/26.
+//
+
+import Testing
+@testable import VoiceSplitr
+
+struct VoiceSplitrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
